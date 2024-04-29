49cded00652e492a3b52b0bd5e92a8c71fda3cd9
oktavianusinsead <oktavianus.it@insead.edu>
2024-04-29 17:46:57 +0700

    update

--- loginproses.php
+++ loginproses.php
@@ -12,52 +12,22 @@ $conn = new mysqli($servername, $username, $password);
 $username=$_POST['username'];
 $password=$_POST['password'];
 
-$login=mysqli_query("SELECT l.*,p.position,c.locationname FROM sky_loginmanager l
+$login="SELECT l.*,p.position,c.locationname FROM sky_loginmanager l
 left outer join sky_position p on p.positionid = l.positionid
 left outer join sky_location c on c.locationid = l.locationid
- WHERE username='".$username."' and password='".$password."'");
-$r=mysqli_fetch_array($login);
-$user=$r['username'];
-$userid=$r['loginid'];
-$location=$r['locationname'];
-$position=$r['position'];
-$fullname=$r['fullname'];
-$positionid=$r['positionid'];
-$locationid=$r['locationid'];
+ WHERE username='".$username."' and password='".$password."'";
 
-if($user=="")
-{
-header("Location: index.php");
-}
-else
-{
-  if($position=="Kasir")
-  {
-$_SESSION['valid'] = true;
-$_SESSION['timeout'] = time();
-$_SESSION['userid'] = $userid;
-$_SESSION['username'] = $user;
-$_SESSION['fullname'] = $fullname;
-$_SESSION['location'] = $location;
-$_SESSION['position'] = $position;
-$_SESSION['locationid'] = $locationid;
-$_SESSION['positionid'] = $positionid;
- header("Location: indexkasir.php");
- }
- else
- {
+$result = $mysqli -> query($login);
 
-$_SESSION['valid'] = true;
-$_SESSION['timeout'] = time();
-$_SESSION['userid'] = $userid;
-$_SESSION['username'] = $user;
-$_SESSION['fullname'] = $fullname;
-$_SESSION['location'] = $location;
-$_SESSION['position'] = $position;
-$_SESSION['locationid'] = $locationid;
-$_SESSION['positionid'] = $positionid;
-header("Location: index2.php");
-}
+// Associative array
+$row = $result -> fetch_assoc();
+printf ("%s (%s)\n", $row["username"], $row["password"]);
+
+// Free result set
+$result -> free_result();
+
+$mysqli -> close();
+
 
 }
 ?>
