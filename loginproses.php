ce9f589aeb11013b296108a7f00c184f40b0e387
oktavianusinsead <oktavianus.it@insead.edu>
2024-04-29 17:47:43 +0700

    update

--- loginproses.php
+++ loginproses.php
@@ -16,18 +16,9 @@ $login="SELECT l.*,p.position,c.locationname FROM sky_loginmanager l
 left outer join sky_position p on p.positionid = l.positionid
 left outer join sky_location c on c.locationid = l.locationid
  WHERE username='".$username."' and password='".$password."'";
-
-$result = $mysqli -> query($login);
-
-// Associative array
-$row = $result -> fetch_assoc();
-printf ("%s (%s)\n", $row["username"], $row["password"]);
-
-// Free result set
-$result -> free_result();
-
-$mysqli -> close();
+echo $login;
 
 
-}
+
+
 ?>
