417b67951c03cd7db262f47487edadb840533849
oktavianusinsead <oktavianus.it@insead.edu>
2024-04-29 17:45:37 +0700

    update

--- loginproses.php
+++ loginproses.php
@@ -1,16 +1,22 @@
 <?php
-include "config.php";
 
-session_start();
 
+//session_start();
+$servername = "202.10.36.23";
+$username = "yellow";
+$password = "yellow123";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+// Check connection
 $username=$_POST['username'];
 $password=$_POST['password'];
 
-$login=mysql_query("SELECT l.*,p.position,c.locationname FROM sky_loginmanager l
+$login=mysqli_query("SELECT l.*,p.position,c.locationname FROM sky_loginmanager l
 left outer join sky_position p on p.positionid = l.positionid
 left outer join sky_location c on c.locationid = l.locationid
  WHERE username='".$username."' and password='".$password."'");
-$r=mysql_fetch_array($login);
+$r=mysqli_fetch_array($login);
 $user=$r['username'];
 $userid=$r['loginid'];
 $location=$r['locationname'];
