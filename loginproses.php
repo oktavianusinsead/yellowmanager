73ddf49ace9907179720e999e95aaeb8e9bceb63
oktavianusinsead <oktavianus.it@insead.edu>
2024-04-29 18:04:01 +0700

    update

--- loginproses.php
+++ loginproses.php
@@ -7,21 +7,31 @@ if ($mysqli -> connect_errno) {
   echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
   
 }
-else{
-  echo"sukses";
-}
 // Check connection
-echo "Data Refresh".$mysqli;
+
 $username=$_POST['username'];
 $password=$_POST['password'];
+
+
+
+
+
+
+// Free result set
+
 
 $login="SELECT l.*,p.position,c.locationname FROM sky_loginmanager l
 left outer join sky_position p on p.positionid = l.positionid
 left outer join sky_location c on c.locationid = l.locationid
  WHERE username='".$username."' and password='".$password."'";
-echo $login;
+$result = $mysqli -> query($login);
 //$result = $conn -> query($login);
-
+// Associative array
+$row = $result -> fetch_assoc();
+printf ("%s (%s)\n", $row["username"], $row["password"]);
 //$row = $result -> fetch_assoc();
 //echo $row["username"];
+$result -> free_result();
+
+$mysqli -> close();
 ?>
